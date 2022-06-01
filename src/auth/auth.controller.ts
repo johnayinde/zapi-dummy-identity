cd53839ccd659e80bc91b5777ce9ab460e612108
unknown <babafemiolasunmade@gmal.com>
2022-06-01 11:39:45 +0100

    signup refactored

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,9 @@
-import { Body, Controller, Get, Post, Req, Session } from '@nestjs/common';
+import { Body, Controller,  Post} from '@nestjs/common';
 import { ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Serialize } from '../intereptors/serialize.interceptor';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from '../user/dto/create-user.dto';
 import { UserDto } from '../user/dto/user.dto';
-import { Request } from 'express';
 
 @ApiTags("Auth-Users")
 @Controller('auth')
@@ -18,11 +17,8 @@ export class AuthController {
     @ApiOperation({description: 'Sign up a User'})
     async signUpUser(
         @Body() body: CreateUserDto,
-        @Req() req: Request
     ){
-        const user = await this.authService.signup(body,{ userAgent: req.headers['user-agent'], ipAddress: req.ip })
+        const user = await this.authService.signup(body)
         return user 
-    }
-    
-
+    }   
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,17 +3,15 @@ import { UsersRepository } from '../database/repository/user.repository';
 import { User } from '../entities/user.entity';
 import { ZuAppResponse } from '../common/helpers/response';
 import { CreateUserDto } from '../user/dto/create-user.dto';
-import { JwtHelperService } from './jwtHelper.service';
 
 
 @Injectable()
 export class AuthService {
     constructor(
-        private usersRepo: UsersRepository,
-        private jwtHelperService: JwtHelperService
+        private usersRepo: UsersRepository
     ){}
 
-    async signup(user: CreateUserDto, values: {userAgent: string, ipAddress: string}){
+    async signup(user: CreateUserDto){
         const userdata = Object.assign(new User(), user)
         const newUser = await this.usersRepo.save(userdata).catch(e => {
             console.log(e)
@@ -21,22 +19,6 @@ export class AuthService {
                 ZuAppResponse.BadRequest("Duplicate Values", "The Email already exists")
             )
         })
-        const tokens = await this.getNewRefreshAndAccessTokens(values, newUser)
-        const response =  ZuAppResponse.Ok<string>("Accepted", "Successfully logged in", 201)
-        return [newUser, tokens.access, response.message]
-    }
-
-    async getNewRefreshAndAccessTokens(values: {userAgent: string, ipAddress: string}, user){
-        const refreshobject = {
-            userAgent: values.userAgent,
-            ipAddress: values.ipAddress,
-            id: user.id
-        }
-    
-        return {
-            access: await this.jwtHelperService.signAccess(refreshobject),
-            refresh: await this.jwtHelperService.signRefresh(refreshobject)
-        }
-    }
-   
+        return newUser
+    }   
 }
