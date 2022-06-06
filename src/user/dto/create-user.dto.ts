854db676302ecb58b4391dee4a4d43d51321d47c
unknown <babafemiolasunmade@gmal.com>
2022-06-06 14:10:58 +0100

    user password validator created in the CreateUserDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,11 +1,23 @@
-import {IsEmail, IsString} from 'class-validator'
+import {IsEmail, IsString, MinLength, Matches, MaxLength, IsNotEmpty} from 'class-validator'
 export class CreateUserDto {
     @IsString()
+    @IsNotEmpty({message: 'fullname cannot be empty'})
     fullName: string
 
     @IsEmail()
+    @IsString()
     email: string
 
     @IsString()
+    @IsNotEmpty({message: 'password cannot be empty'})
+    @MinLength(8, 
+        {message: 'Title is too short. Minimal length is $constraint1 characters, but actual is $value'}
+    )
+    @MaxLength(20, 
+        { message: 'Title is too long. Maximal length is $constraint1 characters, but actual is $value'}
+    )
+    @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
+        {message: 'password must contain the following: a capital letter, a number and a special character'}
+    )
     password: string
 }
