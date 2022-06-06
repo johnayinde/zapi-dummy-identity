ae11461a428cbd6dc3d8d8d8ce5a74de41841b48
unknown <babafemiolasunmade@gmal.com>
2022-06-06 20:09:05 +0100

    password  validator response message corrected

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -11,10 +11,10 @@ export class CreateUserDto {
     @IsString()
     @IsNotEmpty({message: 'password cannot be empty'})
     @MinLength(8, 
-        {message: 'Title is too short. Minimal length is $constraint1 characters, but actual is $value'}
+        {message: 'Password is too short. Minimal length is $constraint1 characters, but actual is $value'}
     )
     @MaxLength(20, 
-        { message: 'Title is too long. Maximal length is $constraint1 characters, but actual is $value'}
+        { message: 'password is too long. Maximal length is $constraint1 characters, but actual is $value'}
     )
     @Matches(/((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, 
         {message: 'password must contain the following: a capital letter, a number and a special character'}
