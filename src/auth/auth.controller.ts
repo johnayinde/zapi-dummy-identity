e27d36a6733e2b8433fa35c8d7df457ac7af8f72
andyjrII <enehizenajames@gmail.com>
2022-06-24 09:58:18 +0100

    Sign in & Sign out correction

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -11,12 +11,12 @@ import { PasswordResetDto } from '../user/dto/password-reset.dto';
 
 @ApiTags("Auth-Users")
 @Controller('auth')
-@Serialize(UserDto)
 export class AuthController {
     constructor(
         private readonly authService: AuthService,
     ){}
 
+    @Serialize(UserDto)
     @Post('/signup')
     @ApiOperation({description: 'Sign up a User'})
     async signUpUser(
@@ -28,19 +28,30 @@ export class AuthController {
 
     @Post('/signin')
     @ApiOperation({description: 'Sign in a User'})
-    async signInUser(
-        @Body() body: SignInDto,
-        @Req() req: Request
-    ){
-        return this.authService.signin(body, { userAgent: req.headers['user-agent'], ipAddress: req.ip })
+    async signInUser(@Body() dto: SignInDto, @Req() req: Request) {
+        return this.authService.signin(dto, { userAgent: req.headers['user-agent'], ipAddress: req.ip });
+    }
+
+    @Post('/signout')
+    @ApiOperation({description: 'Sign out a User'})
+    async signOutUser(@Body('refreshToken') refreshToken: string) {
+        return await this.authService.signout(refreshToken);
+    }
+
+    @Post('/token')
+    @ApiOperation({description: 'Get new access token'})
+    getAccess(@Body('refreshToken') token: string){
+        return this.authService.getNewTokens(token)
     }
     
+    @Serialize(UserDto)
     @Post('/forgot/post')
     @ApiOperation({description:'submit email for password reset'})
     async forgotPassword(@Body() email: string){
         return await this.authService.forgotPassword(email)
     }
 
+    @Serialize(UserDto)
     @Post('/reset/:id/:token')
     @ApiOperation({description: 'password reset function'})
     async resetPassword(
