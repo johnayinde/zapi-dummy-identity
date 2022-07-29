4706157f417eebeaad03d32fd99a7250922f88f1
Ayinde John <lolaayinde@gmail.com>
2022-07-29 10:50:55 +0100

    provided endpoint for user login histories and added documentation

--- src/common/interface/userInfo.interface.ts
+++ src/common/interface/userInfo.interface.ts
@@ -1,7 +1,14 @@
-export interface UserInfo {
+import { ApiProperty } from '@nestjs/swagger';
+
+export class UserInfo {
+  @ApiProperty()
   login_time: string;
+  @ApiProperty()
   country: string;
+  @ApiProperty()
   ip_address: string;
+  @ApiProperty()
   browser_name: string;
+  @ApiProperty()
   os_name: string;
 }
